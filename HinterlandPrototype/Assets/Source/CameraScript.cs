290a5827b947bf2f80257d786bd5a4baedc2844b
Your Name <chooker101@gmail.com>
2016-06-06 16:19:25 -0700

    Initial comit of prototype

---
+++ HinterlandPrototype/Assets/Source/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+	public GameObject player;
+	[SerializeField]
+	private Vector3 place;
+	private Transform cachedtrans;
+	private Transform playertcache;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cachedtrans = this.GetComponent<Transform>();
+		playertcache = player.GetComponent<Transform>();
+		cachedtrans.LookAt(playertcache);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		cachedtrans.position = place + playertcache.position;
+	}
+}
---
+++ HinterlandPrototype/Assets/Source/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+	public float force;
+	private Rigidbody2D cache_rb;
+	private Transform cache_tf;
+	private Animator cachen_anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cache_rb = this.GetComponent<Rigidbody2D>();
+		cache_tf = this.GetComponent<Transform>();
+		cachen_anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector2 move = Vector2.zero;
+		move.x = Input.GetAxis("Horizontal");
+		move.y = Input.GetAxis("Vertical");
+		cachen_anim.SetFloat("Ver", move.y);
+		cachen_anim.SetFloat("Hor", move.x);
+
+		cache_rb.velocity = move * force;
+		
+	}
+}
