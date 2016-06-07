a81727fc635bc13ce1826c5ba94413070fe23b4a
Your Name <chooker101@gmail.com>
2016-06-06 17:27:56 -0700

    Added button

--- HinterlandPrototype/Assets/Source/PlayerScript.cs
+++ HinterlandPrototype/Assets/Source/PlayerScript.cs
@@ -7,10 +7,12 @@ public class PlayerScript : MonoBehaviour
 	private Rigidbody2D cache_rb;
 	private Transform cache_tf;
 	private Animator cachen_anim;
+	private Vector2 move;
 
 	// Use this for initialization
 	void Start ()
 	{
+		move = Vector2.zero;
 		cache_rb = this.GetComponent<Rigidbody2D>();
 		cache_tf = this.GetComponent<Transform>();
 		cachen_anim = this.GetComponent<Animator>();
@@ -19,13 +21,11 @@ public class PlayerScript : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		Vector2 move = Vector2.zero;
 		move.x = Input.GetAxis("Horizontal");
 		move.y = Input.GetAxis("Vertical");
 		cachen_anim.SetFloat("Ver", move.y);
 		cachen_anim.SetFloat("Hor", move.x);
 
 		cache_rb.velocity = move * force;
-		
 	}
 }
