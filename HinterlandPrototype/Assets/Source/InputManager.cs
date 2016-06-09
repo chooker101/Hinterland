869d398d756c99b7201f197f048fafc9010c1bdb
Your Name <chooker101@gmail.com>
2016-06-08 18:53:33 -0700

    Stuff

---
+++ HinterlandPrototype/Assets/Source/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+	public Vector2 move;
+	public bool Touch;
+	public bool UpOn;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Touch)
+		{
+			if(UpOn)
+			{
+				move.y = 1;
+			}
+		}
+		else
+		{
+			move.y = Input.GetAxis("Vertical");
+			move.x = Input.GetAxis("Horizontal");
+		}
+	}
+}
--- HinterlandPrototype/Assets/Source/PlayerScript.cs
+++ HinterlandPrototype/Assets/Source/PlayerScript.cs
@@ -7,12 +7,12 @@ public class PlayerScript : MonoBehaviour
 	private Rigidbody2D cache_rb;
 	private Transform cache_tf;
 	private Animator cachen_anim;
-	private Vector2 move;
+	private InputManager cache_input;
 
 	// Use this for initialization
 	void Start ()
 	{
-		move = Vector2.zero;
+		cache_input = this.GetComponent<InputManager>();
 		cache_rb = this.GetComponent<Rigidbody2D>();
 		cache_tf = this.GetComponent<Transform>();
 		cachen_anim = this.GetComponent<Animator>();
@@ -21,11 +21,11 @@ public class PlayerScript : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		move.x = Input.GetAxis("Horizontal");
-		move.y = Input.GetAxis("Vertical");
-		cachen_anim.SetFloat("Ver", move.y);
-		cachen_anim.SetFloat("Hor", move.x);
+		cache_input.move.x = Input.GetAxis("Horizontal");
+		cache_input.move.y = Input.GetAxis("Vertical");
+		cachen_anim.SetFloat("Ver", cache_input.move.y);
+		cachen_anim.SetFloat("Hor", cache_input.move.x);
 
-		cache_rb.velocity = move * force;
+		cache_rb.velocity = cache_input.move * force;
 	}
 }
