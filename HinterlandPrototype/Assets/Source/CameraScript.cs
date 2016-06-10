bb40938925e8ec1ad3cc98b1ff317a360d943cd6
chooker101 <chooker101@gmail.com>
2016-06-09 22:02:56 -0700

    Added edit mode for the houses

--- HinterlandPrototype/Assets/Source/CameraScript.cs
+++ HinterlandPrototype/Assets/Source/CameraScript.cs
@@ -4,22 +4,47 @@ using System.Collections;
 public class CameraScript : MonoBehaviour
 {
 	public GameObject player;
+	public bool EditMode;
+	public float camspeed;
+	[SerializeField]
+	private float pullbackdist;
 	[SerializeField]
 	private Vector3 place;
-	private Transform cachedtrans;
+	private Transform cache_tf;
+	private Rigidbody2D cache_rb;
 	private Transform playertcache;
+	private InputManager inputcache;
+	private bool once = false;
 
 	// Use this for initialization
 	void Start ()
 	{
-		cachedtrans = this.GetComponent<Transform>();
+		cache_tf = this.GetComponent<Transform>();
+		cache_rb = this.GetComponent<Rigidbody2D>();
 		playertcache = player.GetComponent<Transform>();
-		cachedtrans.LookAt(playertcache);
+		inputcache = player.GetComponent<InputManager>();
+		cache_tf.LookAt(playertcache);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		cachedtrans.position = place + playertcache.position;
+		if (!EditMode)
+		{
+			cache_tf.position = place + playertcache.position;
+			once = true;
+		}
+		else
+		{
+			if(once)
+			{
+				Vector3 temp = cache_tf.position;
+				temp.z -= pullbackdist;
+				temp.y -= pullbackdist;
+				cache_tf.position = temp;
+				once = false;
+			}
+			cache_rb.velocity = inputcache.move * camspeed;
+		}
 	}
 }
--- HinterlandPrototype/Assets/Source/PlayerScript.cs
+++ HinterlandPrototype/Assets/Source/PlayerScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour
 {
 	public float force;
+	public GameObject cam;
+	private CameraScript cs;
 	private Rigidbody2D cache_rb;
 	private Transform cache_tf;
 	private Animator cachen_anim;
@@ -16,14 +18,18 @@ public class PlayerScript : MonoBehaviour
 		cache_rb = this.GetComponent<Rigidbody2D>();
 		cache_tf = this.GetComponent<Transform>();
 		cachen_anim = this.GetComponent<Animator>();
+		cs = cam.GetComponent<CameraScript>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		cachen_anim.SetFloat("Ver", cache_input.move.y);
-		cachen_anim.SetFloat("Hor", cache_input.move.x);
+		if (!cs.EditMode)
+		{
+			cachen_anim.SetFloat("Ver", cache_input.move.y);
+			cachen_anim.SetFloat("Hor", cache_input.move.x);
 
-		cache_rb.velocity = cache_input.move * force;
+			cache_rb.velocity = cache_input.move * force;
+		}
 	}
 }
